c444c71095ca824939a5856dd0e246f743306c10
yangTauTauAjah <tesakun6464@gmail.com>
2023-02-15 21:13:20 +0700

    dark mode

--- pages/projects/rest-country-api/[name].tsx
+++ pages/projects/rest-country-api/[name].tsx
@@ -1,17 +1,261 @@
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { Wrapper } from ".";
+import { useEffect, useMemo } from "react";
+import { COLORS, ThemeContext, Wrapper } from ".";
 import data from "../../../public/rest-country-api/data.json";
 
-const colors = {
-  darkBlue: "#2b3945",
-  veryDarkBlueBg: "#202c37",
-  white: "#ffffff",
-  veryDarkBlueText: "#111517",
-  darkGrayInput: "#858585",
-  veryLightGrayBg: "#fafafa",
+const ALPA3CODE = {
+  AFG: "Afghanistan",
+  ALA: "Åland Islands",
+  ALB: "Albania",
+  DZA: "Algeria",
+  ASM: "American Samoa",
+  AND: "Andorra",
+  AGO: "Angola",
+  AIA: "Anguilla",
+  ATA: "Antarctica",
+  ATG: "Antigua and Barbuda",
+  ARG: "Argentina",
+  ARM: "Armenia",
+  ABW: "Aruba",
+  AUS: "Australia",
+  AUT: "Austria",
+  AZE: "Azerbaijan",
+  BHS: "Bahamas",
+  BHR: "Bahrain",
+  BGD: "Bangladesh",
+  BRB: "Barbados",
+  BLR: "Belarus",
+  BEL: "Belgium",
+  BLZ: "Belize",
+  BEN: "Benin",
+  BMU: "Bermuda",
+  BTN: "Bhutan",
+  BOL: "Bolivia (Plurinational State of)",
+  BES: "Bonaire, Sint Eustatius and Saba",
+  BIH: "Bosnia and Herzegovina",
+  BWA: "Botswana",
+  BVT: "Bouvet Island",
+  BRA: "Brazil",
+  IOT: "British Indian Ocean Territory",
+  UMI: "United States Minor Outlying Islands",
+  VGB: "Virgin Islands (British)",
+  VIR: "Virgin Islands (U.S.)",
+  BRN: "Brunei Darussalam",
+  BGR: "Bulgaria",
+  BFA: "Burkina Faso",
+  BDI: "Burundi",
+  KHM: "Cambodia",
+  CMR: "Cameroon",
+  CAN: "Canada",
+  CPV: "Cabo Verde",
+  CYM: "Cayman Islands",
+  CAF: "Central African Republic",
+  TCD: "Chad",
+  CHL: "Chile",
+  CHN: "China",
+  CXR: "Christmas Island",
+  CCK: "Cocos (Keeling) Islands",
+  COL: "Colombia",
+  COM: "Comoros",
+  COG: "Congo",
+  COD: "Congo (Democratic Republic of the)",
+  COK: "Cook Islands",
+  CRI: "Costa Rica",
+  HRV: "Croatia",
+  CUB: "Cuba",
+  CUW: "Curaçao",
+  CYP: "Cyprus",
+  CZE: "Czech Republic",
+  DNK: "Denmark",
+  DJI: "Djibouti",
+  DMA: "Dominica",
+  DOM: "Dominican Republic",
+  ECU: "Ecuador",
+  EGY: "Egypt",
+  SLV: "El Salvador",
+  GNQ: "Equatorial Guinea",
+  ERI: "Eritrea",
+  EST: "Estonia",
+  ETH: "Ethiopia",
+  FLK: "Falkland Islands (Malvinas)",
+  FRO: "Faroe Islands",
+  FJI: "Fiji",
+  FIN: "Finland",
+  FRA: "France",
+  GUF: "French Guiana",
+  PYF: "French Polynesia",
+  ATF: "French Southern Territories",
+  GAB: "Gabon",
+  GMB: "Gambia",
+  GEO: "Georgia",
+  DEU: "Germany",
+  GHA: "Ghana",
+  GIB: "Gibraltar",
+  GRC: "Greece",
+  GRL: "Greenland",
+  GRD: "Grenada",
+  GLP: "Guadeloupe",
+  GUM: "Guam",
+  GTM: "Guatemala",
+  GGY: "Guernsey",
+  GIN: "Guinea",
+  GNB: "Guinea-Bissau",
+  GUY: "Guyana",
+  HTI: "Haiti",
+  HMD: "Heard Island and McDonald Islands",
+  VAT: "Vatican City",
+  HND: "Honduras",
+  HUN: "Hungary",
+  HKG: "Hong Kong",
+  ISL: "Iceland",
+  IND: "India",
+  IDN: "Indonesia",
+  CIV: "Ivory Coast",
+  IRN: "Iran (Islamic Republic of)",
+  IRQ: "Iraq",
+  IRL: "Ireland",
+  IMN: "Isle of Man",
+  ISR: "Israel",
+  ITA: "Italy",
+  JAM: "Jamaica",
+  JPN: "Japan",
+  JEY: "Jersey",
+  JOR: "Jordan",
+  KAZ: "Kazakhstan",
+  KEN: "Kenya",
+  KIR: "Kiribati",
+  KWT: "Kuwait",
+  KGZ: "Kyrgyzstan",
+  LAO: "Lao People's Democratic Republic",
+  LVA: "Latvia",
+  LBN: "Lebanon",
+  LSO: "Lesotho",
+  LBR: "Liberia",
+  LBY: "Libya",
+  LIE: "Liechtenstein",
+  LTU: "Lithuania",
+  LUX: "Luxembourg",
+  MAC: "Macao",
+  MKD: "North Macedonia",
+  MDG: "Madagascar",
+  MWI: "Malawi",
+  MYS: "Malaysia",
+  MDV: "Maldives",
+  MLI: "Mali",
+  MLT: "Malta",
+  MHL: "Marshall Islands",
+  MTQ: "Martinique",
+  MRT: "Mauritania",
+  MUS: "Mauritius",
+  MYT: "Mayotte",
+  MEX: "Mexico",
+  FSM: "Micronesia (Federated States of)",
+  MDA: "Moldova (Republic of)",
+  MCO: "Monaco",
+  MNG: "Mongolia",
+  MNE: "Montenegro",
+  MSR: "Montserrat",
+  MAR: "Morocco",
+  MOZ: "Mozambique",
+  MMR: "Myanmar",
+  NAM: "Namibia",
+  NRU: "Nauru",
+  NPL: "Nepal",
+  NLD: "Netherlands",
+  NCL: "New Caledonia",
+  NZL: "New Zealand",
+  NIC: "Nicaragua",
+  NER: "Niger",
+  NGA: "Nigeria",
+  NIU: "Niue",
+  NFK: "Norfolk Island",
+  PRK: "Korea (Democratic People's Republic of)",
+  MNP: "Northern Mariana Islands",
+  NOR: "Norway",
+  OMN: "Oman",
+  PAK: "Pakistan",
+  PLW: "Palau",
+  PSE: "Palestine, State of",
+  PAN: "Panama",
+  PNG: "Papua New Guinea",
+  PRY: "Paraguay",
+  PER: "Peru",
+  PHL: "Philippines",
+  PCN: "Pitcairn",
+  POL: "Poland",
+  PRT: "Portugal",
+  PRI: "Puerto Rico",
+  QAT: "Qatar",
+  UNK: "Republic of Kosovo",
+  REU: "Réunion",
+  ROU: "Romania",
+  RUS: "Russian Federation",
+  RWA: "Rwanda",
+  BLM: "Saint Barthélemy",
+  SHN: "Saint Helena, Ascension and Tristan da Cunha",
+  KNA: "Saint Kitts and Nevis",
+  LCA: "Saint Lucia",
+  MAF: "Saint Martin (French part)",
+  SPM: "Saint Pierre and Miquelon",
+  VCT: "Saint Vincent and the Grenadines",
+  WSM: "Samoa",
+  SMR: "San Marino",
+  STP: "Sao Tome and Principe",
+  SAU: "Saudi Arabia",
+  SEN: "Senegal",
+  SRB: "Serbia",
+  SYC: "Seychelles",
+  SLE: "Sierra Leone",
+  SGP: "Singapore",
+  SXM: "Sint Maarten (Dutch part)",
+  SVK: "Slovakia",
+  SVN: "Slovenia",
+  SLB: "Solomon Islands",
+  SOM: "Somalia",
+  ZAF: "South Africa",
+  SGS: "South Georgia and the South Sandwich Islands",
+  KOR: "Korea (Republic of)",
+  ESP: "Spain",
+  LKA: "Sri Lanka",
+  SDN: "Sudan",
+  SSD: "South Sudan",
+  SUR: "Suriname",
+  SJM: "Svalbard and Jan Mayen",
+  SWZ: "Swaziland",
+  SWE: "Sweden",
+  CHE: "Switzerland",
+  SYR: "Syrian Arab Republic",
+  TWN: "Taiwan",
+  TJK: "Tajikistan",
+  TZA: "Tanzania, United Republic of",
+  THA: "Thailand",
+  TLS: "Timor-Leste",
+  TGO: "Togo",
+  TKL: "Tokelau",
+  TON: "Tonga",
+  TTO: "Trinidad and Tobago",
+  TUN: "Tunisia",
+  TUR: "Turkey",
+  TKM: "Turkmenistan",
+  TCA: "Turks and Caicos Islands",
+  TUV: "Tuvalu",
+  UGA: "Uganda",
+  UKR: "Ukraine",
+  ARE: "United Arab Emirates",
+  GBR: "United Kingdom of Great Britain and Northern Ireland",
+  USA: "United States of America",
+  URY: "Uruguay",
+  UZB: "Uzbekistan",
+  VUT: "Vanuatu",
+  VEN: "Venezuela (Bolivarian Republic of)",
+  VNM: "Vietnam",
+  WLF: "Wallis and Futuna",
+  ESH: "Western Sahara",
+  YEM: "Yemen",
+  ZMB: "Zambia",
+  ZWE: "Zimbabwe",
 };
 
 type currency = {
@@ -40,91 +284,22 @@ interface Country {
   languages?: language[];
 }
 
-function Country({ country }: { country?: Country }) {
-  return country ? (
-    <div className="w-auto px-7 py-10">
-      <a
-        href="/"
-        className="rounded flex justify-between items-center px-8 bg-white w-24 h-8 shadow-lg"
-      >
-        {/* @ts-ignore */}
-        <ion-icon size="large" name="arrow-back-outline" />
-        <p className="pointer-event-none">Back</p>
-      </a>
-      <Image
-        src={country.flag}
-        alt="flag"
-        width={1024}
-        height={1024}
-        className="rounded mt-16 w-full shadow-2xl"
-      />
-      <h1 className="mt-12 font-extrabold text-2xl">{country.name}</h1>
-      <div className="mt-9 flex flex-col gap-12">
-        <div className="flex flex-col gap-5 text-lg">
-          <p>
-            <span className="font-semibold">Native Name:</span>{" "}
-            {country.nativeName}
-          </p>
-          <p>
-            <span className="font-semibold">Population:</span>{" "}
-            {country.population}
-          </p>
-          <p>
-            <span className="font-semibold">Region:</span> {country.region}
-          </p>
-          <p>
-            <span className="font-semibold">Sub Region:</span>{" "}
-            {country.subregion}
-          </p>
-          <p>
-            <span className="font-semibold">Capital:</span> {country.capital}
-          </p>
-        </div>
-        <div className="flex flex-col gap-5 text-lg">
-          <p>
-            <span className="font-semibold">Top Level Domain:</span>{" "}
-            {country.topLevelDomain}
-          </p>
-          <p>
-            <span className="font-semibold">Currencies:</span>{" "}
-            {country.currencies?.map((e) => e.name).join(", ")}
-          </p>
-          <p>
-            <span className="font-semibold">Languages:</span>{" "}
-            {country.languages?.map((e) => e.name).join(", ")}
-          </p>
-        </div>
-        <div>
-          <p className="font-semibold text-xl">Border Countries:</p>
-          <ul className="mt-6 flex gap-3">
-            {country?.borders?.map((e, i) => (
-              <li
-                key={i}
-                className="rounded shadow-lg flex place-items-center w-24 h-7"
-              >
-                <p className="w-full text-center">{e}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <h1>404</h1>
-  );
-}
-
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [
       { params: { name: "afghanistan" } },
       { params: { name: "belgium" } },
+      { params: { name: "indonesia" } },
     ],
     fallback: true,
   };
 };
 
-export const getStaticProps = async ({params}: {params: {name: string}}) => {
+export const getStaticProps = async ({
+  params,
+}: {
+  params: { name: string };
+}) => {
   return {
     props: { name: params.name },
   };
@@ -132,16 +307,101 @@ export const getStaticProps = async ({params}: {params: {name: string}}) => {
 
 export default function ({ name }: { name: string }) {
   const router = useRouter();
-
-  console.log(router.query)
-
   if (router.isFallback) return <h1>Loading</h1>;
+
+  const country = useMemo(
+    () => data.find((e) => e.name.match(new RegExp(name, "i"))),
+    []
+  );
 
   return (
     <Wrapper>
-      <Country
-        country={data.find((e) => e.name.match(new RegExp(name, "i")))}
-      />
+      <ThemeContext.Consumer>
+        {({ theme }) =>
+          country ? (
+            <div className="w-auto px-7 py-10">
+              <button
+                onClick={() => router.push('/projects/rest-country-api/indonesia')}
+                style={{
+                  background:
+                    theme === "light" ? COLORS.white : COLORS.darkBlue,
+                }}
+                className="rounded flex justify-between items-center px-8 w-24 h-8 shadow-lg"
+              >
+                {/* @ts-ignore */}
+                <ion-icon size="large" name="arrow-back-outline" />
+                <p className="pointer-event-none">Back</p>
+              </button>
+              <Image
+                src={country.flag}
+                alt="flag"
+                width={1024}
+                height={1024}
+                className="rounded mt-16 w-full shadow-2xl"
+              />
+              <h1 className="mt-12 font-extrabold text-2xl">{country.name}</h1>
+              <div className="mt-9 flex flex-col gap-12">
+                <div className="flex flex-col gap-5 text-lg">
+                  <p>
+                    <span className="font-semibold">Native Name:</span>{" "}
+                    {country.nativeName}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Population:</span>{" "}
+                    {country.population}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Region:</span>{" "}
+                    {country.region}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Sub Region:</span>{" "}
+                    {country.subregion}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Capital:</span>{" "}
+                    {country.capital}
+                  </p>
+                </div>
+                <div className="flex flex-col gap-5 text-lg">
+                  <p>
+                    <span className="font-semibold">Top Level Domain:</span>{" "}
+                    {country.topLevelDomain}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Currencies:</span>{" "}
+                    {country.currencies?.map((e) => e.name).join(", ")}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Languages:</span>{" "}
+                    {country.languages?.map((e) => e.name).join(", ")}
+                  </p>
+                </div>
+                <div>
+                  <p className="font-semibold text-xl">Border Countries:</p>
+                  <ul className="mt-6 flex flex-wrap gap-3">
+                    {country?.borders?.map((e, i) => (
+                      <li
+                        key={i}
+                        style={{
+                          background: theme === 'white' ? COLORS.white : COLORS.darkBlue
+                        }}
+                        className="rounded shadow-lg flex place-items-center px-4 h-7"
+                      >
+                        <p className="w-full text-center">
+                          {ALPA3CODE[e as keyof typeof ALPA3CODE]}
+                        </p>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <h1>404</h1>
+          )
+        }
+      </ThemeContext.Consumer>
     </Wrapper>
   );
 }
--- pages/projects/rest-country-api/index.tsx
+++ pages/projects/rest-country-api/index.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import Down from "../../../public/rest-country-api/down-arrow.png";
-import data from "../../../public/rest-country-api/data.json";
+import { Context, useEffect, useState } from "react";
 import Script from "next/script";
 import { useRouter } from "next/router";
+import Down from "../../../public/rest-country-api/down-arrow.png";
+import data from "../../../public/rest-country-api/data.json";
+import { createContext } from "react";
 
-const colors = {
-  darkBlue: "#2b3945",
-  veryDarkBlueBg: "#202c37",
-  white: "#ffffff",
+export const COLORS = {
+  darkBlue: "#2B3945",
+  veryDarkBlueBg: "#202C37",
+  white: "#FFFFFF",
   veryDarkBlueText: "#111517",
   darkGrayInput: "#858585",
-  veryLightGrayBg: "#fafafa",
+  veryLightGrayBg: "#FAFAFA",
 };
 
 type region = "Africa" | "America" | "Asia" | "Europe" | "Oceania";
@@ -43,27 +44,32 @@ interface Country {
 function Card({ name, flag, population, region, capital }: Country) {
   const router = useRouter();
   return (
-    <li
-      onClick={() => router.push(`/projects/rest-country-api/${name}`)}
-      className="flex flex-col justify-between rounded-lg bg-white shadow-lg w-64 overflow-hidden"
-    >
-      <img src={flag} alt={`${name} flag`} className="w-full" />
-      <div className="px-6 py-8">
-        <h3 className="text-xl font-extrabold">{name}</h3>
-        <p className="mt-6">
-          <span className="font-semibold">population : </span>
-          {population?.toLocaleString("id")}
-        </p>
-        <p>
-          <span className="font-semibold">Region : </span>
-          {region}
-        </p>
-        <p>
-          <span className="font-semibold">Capital : </span>
-          {capital}
-        </p>
-      </div>
-    </li>
+    <ThemeContext.Consumer>
+      {({theme}) => (
+        <li
+          onClick={() => router.push(`/projects/rest-country-api/${name}`)}
+          style={{background: theme === "light" ? COLORS.white : COLORS.darkBlue}}
+          className="flex flex-col justify-between rounded-lg shadow-lg w-64 overflow-hidden"
+        >
+          <img src={flag} alt={`${name} flag`} className="w-full" />
+          <div className="px-6 py-8">
+            <h3 className="text-xl font-extrabold">{name}</h3>
+            <p className="mt-6">
+              <span className="font-semibold">population : </span>
+              {population?.toLocaleString("id")}
+            </p>
+            <p>
+              <span className="font-semibold">Region : </span>
+              {region}
+            </p>
+            <p>
+              <span className="font-semibold">Capital : </span>
+              {capital}
+            </p>
+          </div>
+        </li>
+      )}
+    </ThemeContext.Consumer>
   );
 }
 
@@ -78,84 +84,99 @@ function Select({
   const [show, setShow] = useState(false);
 
   return (
-    <div tabIndex={0} className="relative">
-      <div
-        onClick={() => setShow(!show)}
-        className="rounded-md flex justify-between items-center w-52 h-12 shadow-md px-6 font-semibold"
-      >
-        <p className="pointer-events-none">{filter || "Filter by region"}</p>
-        <div className="aspect-square h-3">
-          <Image src={Down} alt="drop-down" height={50} width={50} />
+    <ThemeContext.Consumer>
+      {({theme}) => (
+        <div tabIndex={0} className="relative">
+          <div
+            onClick={() => setShow(!show)}
+            style={{background: theme === "light" ? COLORS.white : COLORS.darkBlue}}
+            className="rounded-md flex justify-between items-center w-52 h-12 shadow-md px-6 font-semibold"
+          >
+            <p className="pointer-events-none">{filter || "Filter by region"}</p>
+            <div className="aspect-square h-3">
+              <Image src={Down} alt="drop-down" height={50} width={50} />
+            </div>
+          </div>
+          <ul
+            style={{background: theme === "light" ? COLORS.white : COLORS.darkBlue}}
+            className={`${
+              show ? "flex" : "hidden"
+            } flex-col items-start rounded-md overflow-hidden absolute top-12 mt-1 w-52 shadow-md outline-none font-semibold`}
+          >
+            <li
+              onClick={() => {
+                setFilter("Africa");
+                setShow(false);
+              }}
+              className={`hover:bg-[rgba(0,0,0,.2)] w-full px-6 py-3`}
+            >
+              <p className="pointer-events-none">Africa</p>
+            </li>
+            <li
+              onClick={() => {
+                setFilter("Americas");
+                setShow(false);
+              }}
+              className={`hover:bg-[rgba(0,0,0,.2)] w-full px-6 py-3`}
+            >
+              <p className="pointer-events-none">Americas</p>
+            </li>
+            <li
+              onClick={() => {
+                setFilter("Asia");
+                setShow(false);
+              }}
+              className={`hover:bg-[rgba(0,0,0,.2)] w-full px-6 py-3`}
+            >
+              <p className="pointer-events-none">Asia</p>
+            </li>
+            <li
+              onClick={() => {
+                setFilter("Europe");
+                setShow(false);
+              }}
+              className={`hover:bg-[rgba(0,0,0,.2)] w-full px-6 py-3`}
+            >
+              <p className="pointer-events-none">Europe</p>
+            </li>
+            <li
+              onClick={() => {
+                setFilter("Oceania");
+                setShow(false);
+              }}
+              className={`hover:bg-[rgba(0,0,0,.2)] w-full px-6 py-3`}
+            >
+              <p className="pointer-events-none">Oceania</p>
+            </li>
+          </ul>
         </div>
-      </div>
-      <ul
-        className={`${
-          show ? "flex" : "hidden"
-        } flex-col items-start rounded-md overflow-hidden absolute bg-white top-12 mt-1 w-52 shadow-md outline-none font-semibold`}
-      >
-        <li
-          onClick={() => {
-            setFilter("Africa");
-            setShow(false);
-          }}
-          className={`hover:bg-[#e5e5e5] w-full px-6 py-3`}
-        >
-          <p className="pointer-events-none">Africa</p>
-        </li>
-        <li
-          onClick={() => {
-            setFilter("Americas");
-            setShow(false);
-          }}
-          className={`hover:bg-[#e5e5e5] w-full px-6 py-3`}
-        >
-          <p className="pointer-events-none">Americas</p>
-        </li>
-        <li
-          onClick={() => {
-            setFilter("Asia");
-            setShow(false);
-          }}
-          className={`hover:bg-[#e5e5e5] w-full px-6 py-3`}
-        >
-          <p className="pointer-events-none">Asia</p>
-        </li>
-        <li
-          onClick={() => {
-            setFilter("Europe");
-            setShow(false);
-          }}
-          className={`hover:bg-[#e5e5e5] w-full px-6 py-3`}
-        >
-          <p className="pointer-events-none">Europe</p>
-        </li>
-        <li
-          onClick={() => {
-            setFilter("Oceania");
-            setShow(false);
-          }}
-          className={`hover:bg-[#e5e5e5] w-full px-6 py-3`}
-        >
-          <p className="pointer-events-none">Oceania</p>
-        </li>
-      </ul>
-    </div>
+      )}
+    </ThemeContext.Consumer>
   );
 }
 
 function List() {
   const [filter, setFilter] = useState<string | null>(null);
+  const [search, setSearch] = useState<string>("");
 
   return (
     <div className="flex flex-col gap-10 px-4 py-6 items-start">
-      <div className="flex justify-between gap-8 w-full items-center h-12 px-8 bg-white rounded-lg shadow-md">
-        {/* @ts-ignore */}
-        <ion-icon class="text-xl" name="search-outline" />
-        <input
-          placeholder="Search for a country ..."
-          className="outline-none font-semibold h-full flex-grow"
-        />
-      </div>
+      <ThemeContext.Consumer>
+        {({theme}) => (
+          <div
+            style={{background: theme === "light" ? COLORS.white : COLORS.darkBlue}}
+            className="flex justify-between gap-8 w-full items-center h-12 px-8 rounded-lg shadow-md"
+          >
+            {/* @ts-ignore */}
+            <ion-icon class="text-xl" name="search-outline" />
+            <input
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search for a country ..."
+              className="outline-none font-semibold h-full bg-transparent flex-grow"
+            />
+          </div>
+        )}
+      </ThemeContext.Consumer>
 
       <Select state={[filter, setFilter]} />
 
@@ -167,6 +188,10 @@ function List() {
           let _temp = data;
 
           if (filter) _temp = _temp.filter((e) => e.region === filter);
+          if (search !== "")
+            _temp = _temp.filter((e) =>
+              e.name.match(new RegExp(`.*${search}`, "i"))
+            );
 
           return (
             _temp
@@ -188,27 +213,62 @@ function List() {
   );
 }
 
+export const ThemeContext: Context<{ theme: string; toggleTheme: () => any }> =
+  createContext({ theme: "light", toggleTheme: () => {} });
+
 export function Wrapper({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState("light");
   useEffect(() => {
-    document.body.style.setProperty("background", colors.veryLightGrayBg);
+    document.body.style.setProperty("min-height", "100vh");
+    console.log('test')
   }, []);
 
+  useEffect(() => {
+    document.body.style.setProperty("background", theme === 'light' ? COLORS.veryLightGrayBg : COLORS.veryDarkBlueBg);
+  }, [theme])
+
   return (
-    <>
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme: () =>
+          theme === "light" ? setTheme("dark") : setTheme("light"),
+      }}
+    >
       <article
         className="text-sm"
         style={{
-          color: colors.veryDarkBlueText,
+          color: theme === "light" ? COLORS.veryDarkBlueText : COLORS.white,
           fontFamily: "'Nunito Sans', sans-serif",
         }}
       >
-        <header className="flex justify-between items-center h-20 px-4 bg-white shadow-md">
+        <header
+          style={{
+            background:
+              theme === "light" ? COLORS.white : COLORS.darkBlue,
+          }}
+          className="flex justify-between items-center h-20 px-4 shadow-md"
+        >
           <h1 className="text-lg font-extrabold">Where in the world?</h1>
-          <div className="flex items-center gap-2">
-            {/* @ts-ignore */}
-            <ion-icon class="text-xl" name="moon-outline"></ion-icon>
-            <p className="font-semibold">Dark Mode</p>
-          </div>
+          <button
+            onClick={() => {
+              theme === "light" ? setTheme("dark") : setTheme("light");
+            }}
+            className="flex items-center gap-2"
+          >
+            {
+              theme === "light" ? (
+                /* @ts-ignore */
+                <ion-icon class="text-xl" name="moon-outline" />
+              ) : (
+                /* @ts-ignore */
+                <ion-icon name="sunny-outline"></ion-icon>
+              )
+            }
+            <p className="pointer-events-none font-semibold">
+              {theme === "light" ? "Dark" : "Light"} Mode
+            </p>
+          </button>
         </header>
         {children}
       </article>
@@ -220,7 +280,7 @@ export function Wrapper({ children }: { children: React.ReactNode }) {
         noModule
         src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
       />
-    </>
+    </ThemeContext.Provider>
   );
 }
 
