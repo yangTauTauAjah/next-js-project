1650de19aac3b04482473fba9c866668d4d26df1
yangTauTauAjah <tesakun6464@gmail.com>
2023-04-16 18:21:31 +0700

    moving collection data to Collection.ts

---
+++ components/Main/Collection.ts
@@ -0,0 +1,213 @@
+
+
+export const Projects = [
+  {
+    thumbnail: '/src/img/project/NftLandingPage.png',
+    link: 'https://genuine-marshmallow-2e78ee.netlify.app/',
+    source: 'https://github.com/yangTauTauAjah/nft-page',
+    tags: ['React', 'Material UI', 'NFT', 'Front end']
+  },
+  {
+    thumbnail: '/src/img/project/EcommerceProductPage.jpg',
+    link: 'https://next-js-project-ruddy.vercel.app/projects/ecommerce-product-page',
+    source: 'https://github.com/yangTauTauAjah/next-js-project/blob/main/pages/projects/ecommerce-product-page.tsx',
+    tags: ['React', 'Responsive', 'Ecommerce', 'Tailwind']
+  },
+  {
+    thumbnail: '/src/img/project/MultiStepForm.jpg',
+    link: 'https://next-js-project-ruddy.vercel.app/projects/multi-step-form',
+    source: 'https://github.com/yangTauTauAjah/next-js-project/blob/main/pages/projects/multi-step-form.tsx',
+    tags: ['React', 'Responsive', 'Tailwind', 'Form']
+  },
+  {
+    thumbnail: '/src/img/project/RestCountryAPI.jpg',
+    link: 'https://next-js-project-ruddy.vercel.app/projects/rest-country-api?page=1',
+    source: 'https://github.com/yangTauTauAjah/next-js-project/tree/main/pages/projects/rest-country-api',
+    tags: ['React', 'Responsive', 'Rest API', 'Tailwind', 'Dark', 'Light']
+  },
+  {
+    thumbnail: '/src/img/project/ReactMovieApp.png',
+    link: 'https://reactmovieapp.tugasonly.repl.co/',
+    source: 'https://github.com/yangTauTauAjah/reactMovieApp',
+    tags: ['React', 'Fullstack', 'MERN', 'Responsive', 'Ant Design']
+  },
+  {
+    thumbnail: '/src/img/project/TodoApp.jpg',
+    link: 'https://todoapp.tugasonly.repl.co/',
+    source: 'https://github.com/yangTauTauAjah/todoApp',
+    tags: ['Fullstack', 'Native', 'MongoDB', 'Express', 'Responsive']
+  }
+]
+
+export const Accounts = [
+  {
+    color: '#E34C41',
+    thumbnail: "/src/img/png/Gmail_Logo.png",
+    thumbnail_alt: "Gmail_Logo",
+    value: 'habibanwash2603@gmail.com'
+  },
+  {
+    color: '#007AB3',
+    href: 'https://www.linkedin.com/in/habib-anwash-320779244/',
+    thumbnail: "/src/img/png/Linkedin-Logo.png",
+    thumbnail_alt: "Linkedin_Logo",
+    value: 'Habib Anwash'
+  },
+  {
+    color: '#333333',
+    href: 'https://github.com/yangTauTauAjah',
+    thumbnail: "/src/img/png/GitHub_Logo.png",
+    thumbnail_alt: "GitHub_Logo",
+    value: 'yangTauTauAjah'
+  },
+  /* {
+    color: '#24A1DD',
+    href: 'https://t.me/HabibAnwash',
+    thumbnail: "/src/img/png/Telegram_Logo.png",
+    thumbnail_alt: "Telegram_Logo",
+    value: 'Habib Anwash'
+  }, */
+  {
+    color: '#5865F2',
+    href: 'https://discordapp.com/users/372738941155147777',
+    thumbnail: "/src/img/png/Discord_Logo.png",
+    thumbnail_alt: "Discord_Logo.png",
+    value: 'HabibAnwash1447@7305'
+  },
+  {
+    color: '#3FE35C',
+    href: 'https://wa.me/6281217675797',
+    thumbnail: "/src/img/png/Whatsapp_Logo.png",
+    thumbnail_alt: "Whatsapp_Logo",
+    value: '+62 812-1767-5797'
+  }
+]
+
+export const Technologies = [
+  {
+    type: 'Back End',
+    tech: [
+      {
+        logo: '/src/img/png/Node_Logo.png',
+        logo_alt: 'Node_Logo',
+        name: 'Node JS'
+      },
+      {
+        logo: '/src/img/logo/Express_Logo.png',
+        logo_alt: 'Express_Logo',
+        name: 'Express js'
+      },
+      {
+        logo: '/src/img/png/Graphql_Logo.png',
+        logo_alt: 'Graphql_Logo',
+        name: 'GraphQL'
+      },
+      {
+        logo: '/src/img/png/Rest_Logo.png',
+        logo_alt: 'Rest_Logo',
+        name: 'REST API'
+      },
+      {
+        logo: '/src/img/png/Jest_Logo.png',
+        logo_alt: 'Jest_Logo',
+        name: 'Jest'
+      },
+      {
+        logo: '/src/img/png/MongoDB_Logo.png',
+        logo_alt: 'MongoDB_Logo',
+        name: 'MongoDB'
+      },
+      {
+        logo: '/src/img/logo/Mysql_Logo.png',
+        logo_alt: 'MySQL_Logo',
+        name: 'MySQL'
+      }
+    ],
+    bgColor: '#1E5DFF',
+    borderColor: '#1351F1'
+  },
+  {
+    type: 'Front end',
+    tech: [
+      {
+        logo: '/src/img/png/React_Logo.png',
+        logo_alt: 'React_Logo',
+        name: 'React'
+      },
+      {
+        logo: '/src/img/png/Redux_Logo.png',
+        logo_alt: 'Redux',
+        name: 'Redux'
+      },
+      {
+        logo: '/src/img/png/Vite_Logo.png',
+        logo_alt: 'Vite_Logo',
+        name: 'Vite'
+      },
+      {
+        logo: '/src/img/png/Mui_Logo.png',
+        logo_alt: 'Mui_Logo',
+        name: 'Material UI'
+      },
+      {
+        logo: '/src/img/png/Tailwind_Logo.png',
+        logo_alt: 'Tailwind_Logo',
+        name: 'Tailwind CSS'
+      },
+      {
+        logo: '/src/img/png/Antdesign_Logo.png',
+        logo_alt: 'Antdesign_Logo',
+        name: 'Ant Design'
+      },
+      {
+        logo: '/src/img/png/Jquery_Logo.png',
+        logo_alt: 'Jquery_Logo',
+        name: 'JQuery'
+      }
+    ],
+    bgColor: '#363F4D',
+    borderColor: '#33363C'
+  },
+  {
+    type: 'Other tools and languages',
+    tech: [
+      {
+        logo: '/src/img/png/Next_Logo.png',
+        logo_alt: 'Next_Logo',
+        name: 'Next JS'
+      },
+      {
+        logo: '/src/img/png/Vscode_Logo.png',
+        logo_alt: 'Vscode_Logo',
+        name: 'VS Code'
+      },
+      {
+        logo: '/src/img/logo/Js_Logo.jpg',
+        logo_alt: 'Js_Logo',
+        name: 'Javascript'
+      },
+      {
+        logo: '/src/img/png/Typescript_Logo.png',
+        logo_alt: 'Typescript_Logo',
+        name: 'Typescript'
+      },
+      {
+        logo: '/src/img/png/Java_Logo.png',
+        logo_alt: 'Java_Logo',
+        name: 'Java'
+      },
+      {
+        logo: '/src/img/png/Git_Logo.png',
+        logo_alt: 'Git_Logo',
+        name: 'Git VCS'
+      },
+      {
+        logo: '/src/img/png/Figma_Logo.png',
+        logo_alt: 'Figma_Logo',
+        name: 'Figma'
+      }
+    ],
+    bgColor: '#27FF7E',
+    borderColor: '#17D061'
+  }
+]
